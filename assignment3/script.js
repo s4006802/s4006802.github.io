df09f1eafa238b2eee69b0ca1be7a004fb4f2b61
s4006802 <130958538+s4006802@users.noreply.github.com>
2024-10-21 14:38:56 +1100

    added hover

--- assignment3/script.js
+++ assignment3/script.js
@@ -7,6 +7,9 @@ console.log(astroButton);
 const painting = document.querySelectorAll(".painting");
 const astroTag = document.querySelector("#astro-tag")
 console.log(astroTag)
+const artTag = document.querySelectorAll("#artTag");
+const salmonTag = document.querySelector("#salmon-tag")
+
 
 astroBottle.addEventListener("mouseenter", showAstroInfo);
 astroBottle.addEventListener("mouseleave", hideAstroInfo);
@@ -33,9 +36,59 @@ salmonEgg.addEventListener("mouseenter", showSalmonInfo);
 salmonEgg.addEventListener("mouseleave", hideSalmonInfo);
 
 function showSalmonInfo() {
+  salmonTag.classList.add("show")
   console.log("show salmon");
 }
 
 function hideSalmonInfo() {
+  salmonTag.classList.remove("show")
   console.log("hide salmon");
+}
+
+const king = document.querySelector("#king");
+const kingTag = document.querySelector("#king-tag")
+
+king.addEventListener("mouseenter", showKingInfo);
+king.addEventListener("mouseleave", hideKingInfo);
+
+function showKingInfo() {
+  kingTag.classList.add("show")
+  console.log("show king");
+}
+
+function hideKingInfo() {
+  kingTag.classList.remove("show")
+  console.log("hide king");
+}
+
+const duck = document.querySelector("#duck");
+const duckTag = document.querySelector("#duck-tag")
+
+duck.addEventListener("mouseenter", showDuckInfo);
+duck.addEventListener("mouseleave", hideDuckInfo);
+
+function showDuckInfo() {
+  duckTag.classList.add("show")
+  console.log("show duck");
+}
+
+function hideDuckInfo() {
+  duckTag.classList.remove("show")
+  console.log("hide duck");
+}
+
+const sushi = document.querySelector("#tuna-sushi");
+const sushiTag = document.querySelector("#sushi-tag")
+
+sushi.addEventListener("mouseenter", showSushiInfo);
+sushi.addEventListener("mouseleave", hideSushiInfo);
+
+function showSushiInfo() {
+  sushiTag.classList.add("show")
+  console.log("show sushi");
+}
+
+function hideSushiInfo() {
+  sushiTag.classList.remove("show")
+  console.log("hide sushi");
 }
